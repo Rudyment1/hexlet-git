54aa5c6e5edd489dfee0a696cb001613fa6c2b7a
Daniel <dvojtenko969@gmail.com>
2025-06-15 16:07:43 +0900

    New logic for warnings, points and timer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,16 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             textBox1 = new TextBox();
             button1 = new Button();
             button2 = new Button();
-            button3 = new Button();
             button4 = new Button();
+            button3 = new Button();
             textBox2 = new TextBox();
             button5 = new Button();
             comboBox1 = new ComboBox();
             comboBox2 = new ComboBox();
+            ContractionTimer = new System.Windows.Forms.Timer(components);
+            textBox3 = new TextBox();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
             SuspendLayout();
             // 
             // textBox1
@@ -45,7 +51,7 @@
             textBox1.AccessibleRole = AccessibleRole.None;
             textBox1.AutoCompleteMode = AutoCompleteMode.Suggest;
             textBox1.Font = new Font("Segoe UI", 45F);
-            textBox1.Location = new Point(676, 36);
+            textBox1.Location = new Point(535, 52);
             textBox1.MaxLength = 2;
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(73, 87);
@@ -53,7 +59,7 @@
             textBox1.TabStop = false;
             textBox1.Text = "0";
             textBox1.TextAlign = HorizontalAlignment.Center;
-            textBox1.KeyPress += textBox1_KeyPress;
+            textBox1.KeyPress += NumericTextBox_KeyPress;
             // 
             // button1
             // 
@@ -61,13 +67,13 @@
             button1.BackgroundImageLayout = ImageLayout.Zoom;
             button1.Cursor = Cursors.Hand;
             button1.FlatStyle = FlatStyle.Popup;
-            button1.Location = new Point(755, 36);
+            button1.Location = new Point(614, 46);
             button1.Name = "button1";
             button1.Size = new Size(75, 48);
             button1.TabIndex = 1;
             button1.TabStop = false;
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            button1.Click += IncreaseScore_Click;
             // 
             // button2
             // 
@@ -75,27 +81,13 @@
             button2.BackgroundImageLayout = ImageLayout.Zoom;
             button2.Cursor = Cursors.Hand;
             button2.FlatStyle = FlatStyle.Popup;
-            button2.Location = new Point(755, 79);
+            button2.Location = new Point(614, 95);
             button2.Name = "button2";
             button2.Size = new Size(75, 48);
             button2.TabIndex = 2;
             button2.TabStop = false;
             button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
-            button3.BackgroundImageLayout = ImageLayout.Zoom;
-            button3.Cursor = Cursors.Hand;
-            button3.FlatStyle = FlatStyle.Popup;
-            button3.Location = new Point(755, 376);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 48);
-            button3.TabIndex = 5;
-            button3.TabStop = false;
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
+            button2.Click += DecreaseScore_Click;
             // 
             // button4
             // 
@@ -103,20 +95,34 @@
             button4.BackgroundImageLayout = ImageLayout.Zoom;
             button4.Cursor = Cursors.Hand;
             button4.FlatStyle = FlatStyle.Popup;
-            button4.Location = new Point(755, 333);
+            button4.Location = new Point(614, 392);
             button4.Name = "button4";
             button4.Size = new Size(75, 48);
-            button4.TabIndex = 4;
+            button4.TabIndex = 5;
             button4.TabStop = false;
             button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
+            button4.Click += DecreaseScore_Click;
+            // 
+            // button3
+            // 
+            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
+            button3.BackgroundImageLayout = ImageLayout.Zoom;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Location = new Point(614, 349);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 48);
+            button3.TabIndex = 4;
+            button3.TabStop = false;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += IncreaseScore_Click;
             // 
             // textBox2
             // 
             textBox2.AccessibleRole = AccessibleRole.None;
             textBox2.AutoCompleteMode = AutoCompleteMode.Suggest;
             textBox2.Font = new Font("Segoe UI", 45F);
-            textBox2.Location = new Point(676, 333);
+            textBox2.Location = new Point(535, 349);
             textBox2.MaxLength = 2;
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(73, 87);
@@ -127,7 +133,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(27, 397);
+            button5.Location = new Point(20, 413);
             button5.Name = "button5";
             button5.Size = new Size(75, 23);
             button5.TabIndex = 6;
@@ -139,7 +145,7 @@
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Нет", "0", "1", "2" });
-            comboBox1.Location = new Point(676, 133);
+            comboBox1.Location = new Point(535, 149);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(73, 23);
             comboBox1.TabIndex = 7;
@@ -150,28 +156,77 @@
             comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "Нет", "0", "1", "2" });
-            comboBox2.Location = new Point(676, 304);
+            comboBox2.Location = new Point(535, 320);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(73, 23);
             comboBox2.TabIndex = 8;
-            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // ContractionTimer
+            // 
+            ContractionTimer.Interval = 1000;
+            ContractionTimer.Tick += ContractionTimer_Tick;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(178, 181);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(235, 135);
+            textBox3.TabIndex = 9;
+            textBox3.Text = "3:00";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            textBox3.TextChanged += textBox3_TextChanged;
+            textBox3.KeyPress += textBox3_KeyPress;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(431, 181);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 10;
+            button6.Text = "Старт";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(431, 237);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 11;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(431, 292);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 12;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AcceptButton = button5;
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(851, 450);
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(710, 455);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(textBox3);
             Controls.Add(comboBox2);
             Controls.Add(comboBox1);
             Controls.Add(button5);
-            Controls.Add(button3);
             Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(textBox2);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -179,12 +234,17 @@
         #endregion
         private TextBox textBox1;
         private Button button2;
-        private Button button3;
         private Button button4;
+        private Button button3;
         private TextBox textBox2;
         private Button button5;
         internal Button button1;
         private ComboBox comboBox1;
         private ComboBox comboBox2;
+        private System.Windows.Forms.Timer ContractionTimer;
+        private TextBox textBox3;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,127 +1,156 @@
+using System.Windows.Forms;
+
 namespace Scoreboard
 {
     public partial class Form1 : Form
-    {
-        int previousWarning = 0;
+    {   // Перменная нужная для сохранения предыдущего предупреждения синего
+        private int _previousBlueWarning = 0;
+        private int _previousRedWarning = 0;
+        // Переменная хранящее начальное время
+        private int _remainingSeconds = 180;
+        // Запущен ли таймер
+        private bool _isInternalChange = false;
         public Form1()
         {
             InitializeComponent();
         }
 
 
-
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        private void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (char.IsDigit(e.KeyChar))
+            // Фильтр ввода в счет
+            if (e.KeyChar == '.' || !char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back &&
+               e.KeyChar != (char)Keys.Delete)
             {
-                return;
+                e.Handled = true;
             }
-            if (char.IsControl(e.KeyChar))
-            {
-                return;
-            }
-            e.Handled = true;
         }
 
 
-
-        private void button1_Click(object sender, EventArgs e)
+        private void ModifyScore(TextBox textBox, int change)
         {
-            int _number = int.Parse(textBox1.Text);
-            _number++;
-            textBox1.Text = _number.ToString();
-        }
-
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int _number = int.Parse(textBox1.Text);
-
-            if (_number == 0)
+            if (int.TryParse(textBox.Text, out int currentScore))
             {
-                return;
-            }
-            else
-            {
-                _number--;
-                textBox1.Text = _number.ToString();
+                int newScore = Math.Max(0, currentScore + change);
+                textBox.Text = newScore.ToString();
             }
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void IncreaseScore_Click(object sender, EventArgs e)
         {
-            int _number = int.Parse(textBox2.Text);
-            _number++;
-            textBox2.Text = _number.ToString();
+            var button = (Button)sender;
+            var associatedTextBox = button == button1 || button == button2 ? textBox1 : textBox2;
+            ModifyScore(associatedTextBox, 1);
         }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void DecreaseScore_Click(object sender, EventArgs e)
         {
-            int _number = int.Parse(textBox2.Text);
-
-            if (_number == 0)
-            {
-                return;
-            }
-            else
-            {
-                _number--;
-                textBox2.Text = _number.ToString();
-            }
+            var button = (Button)sender;
+            var associatedTextBox = button == button1 || button == button2 ? textBox1 : textBox2;
+            ModifyScore(associatedTextBox, -1);
         }
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            int currentScore = int.TryParse(textBox1.Text, out int n) ? n : 0;
 
-            if (int.TryParse(comboBox1.SelectedItem.ToString(), out int _selectedState))
+            if (int.TryParse(comboBox1.SelectedItem?.ToString(), out int selectedWarning))
             {
-                if (_selectedState == 0 && previousWarning == 1 || _selectedState == 1 && previousWarning == 2)
-                {
-                    previousWarning = _selectedState;
-                    int _number = int.Parse(textBox1.Text);
-                    if (_number != 0)
-                    {
-                        _number--;
-                        textBox1.Text = _number.ToString();
-                    }
-                }
-                else if (previousWarning == 0 && _selectedState == 1)
-                {
-                    previousWarning = _selectedState;
-                    int _number = int.Parse(textBox1.Text);
-                    _number++;
-                    textBox1.Text = _number.ToString();
-                }
-                else if (previousWarning == 1 && _selectedState == 2)
-                {
-                    previousWarning = _selectedState;
-                    int _number = int.Parse(textBox1.Text);
-                    _number++;
-                    textBox1.Text = _number.ToString();
-                }
-                else if (previousWarning == 0 && _selectedState == 2)
-                {
-                    previousWarning = _selectedState;
-                    int _number = int.Parse(textBox1.Text);
-                    _number += 2;
-                    textBox1.Text = _number.ToString();
-                }
-                else if (previousWarning == 2 && _selectedState == 0)
-                {
-                    previousWarning = _selectedState;
-                    int _number = int.Parse(textBox1.Text);
-                    if (_number != 0)
-                    {
-                        _number -= 2;
-                        textBox1.Text = _number.ToString();
-                    }
-                }
+                int delta = selectedWarning - _previousBlueWarning;
+                currentScore += delta;
+                _previousBlueWarning = selectedWarning;
+            }
+            else
+            {
+                currentScore -= _previousBlueWarning;
+                _previousBlueWarning = 0;
+            }
+
+            textBox1.Text = Math.Max(0, currentScore).ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!ContractionTimer.Enabled && !textBox3.Text.Contains('_'))
+            {
+                ContractionTimer.Start();
+                button6.Text = "Стоп";
+            }
+            else if (ContractionTimer.Enabled)
+            {
+                ContractionTimer.Stop();
+                button6.Text = "Старт";
+            }
+            else
+            {
+                MessageBox.Show("Введите время полностью в формате m:ss");
             }
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void ContractionTimer_Tick(object sender, EventArgs e)
         {
+            if (_remainingSeconds > 0)
+            {
+                _remainingSeconds--;
 
+
+                textBox3.Text = $"{(_remainingSeconds / 60)}:{(_remainingSeconds % 60):D2}";
+            }
+            else
+            {
+                ContractionTimer.Stop();
+                System.Media.SystemSounds.Beep.Play();
+            }
         }
+
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back &&
+                e.KeyChar != (char)Keys.Delete && e.KeyChar != ':')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (_isInternalChange) return;
+
+            try
+            {
+                _isInternalChange = true;
+                string originalText = textBox3.Text;
+                int originalCursor = textBox3.SelectionStart;
+                string digits = new string(originalText.Where(c => char.IsDigit(c)).ToArray());
+
+
+                string formatted = "_:__";
+
+                if (digits.Length >= 1)
+                    formatted = digits[0] + ":__";
+                if (digits.Length >= 2)
+                    formatted = digits[0] + ":" + digits[1] + "_";
+                if (digits.Length >= 3)
+                    formatted = digits[0] + ":" + digits[1] + digits[2];
+
+                textBox3.Text = formatted;
+
+                if (!formatted.Contains('_'))
+                {
+                    int minutes = int.Parse(formatted[0].ToString());
+                    int seconds = int.Parse(formatted.Substring(2, 2));
+                    _remainingSeconds = minutes * 60 + seconds;
+                }
+
+                int nextCursor = formatted.IndexOf('_');
+                if (nextCursor == -1) nextCursor = formatted.Length;
+
+                textBox3.SelectionStart = nextCursor;
+            }
+            finally
+            {
+                _isInternalChange = false;
+            }
+        }
+
     }
 }
